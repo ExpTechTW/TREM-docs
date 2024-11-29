ee271ddeb4579edfd5ae1563502d4c4ac6ff2478
YuYu1015 <whes1015@gmail.com>
2024-11-29 23:21:13 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# NKUST iOS Club Documentation
+
+這裡是 NKUST iOS Club 的原始碼儲存庫
---
+++ docusaurus.config.ts
@@ -0,0 +1,130 @@
+import type * as Preset from "@docusaurus/preset-classic";
+import type { Config } from "@docusaurus/types";
+import { themes as prismThemes } from "prism-react-renderer";
+
+const config: Config = {
+  title: "NKUST iOS Club",
+  tagline: "NKUST iOS Club",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://NKUST-iOS-Club.github.io",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "NKUST-iOS-Club", // Usually your GitHub org/user name.
+  projectName: "docs", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "throw",
+  onDuplicateRoutes: "throw",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "zh-Hant",
+    locales: ["en", "zh-Hant", "ja"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: "./sidebars.ts",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/NKUST-iOS-Club/docs/tree/main/",
+          editLocalizedFiles: true,
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/NKUST-iOS-Club/docs/tree/main/",
+          editLocalizedFiles: true,
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    // image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: "NKUST iOS Club",
+      logo: {
+        alt: "NKUST iOS Club Logo",
+        src: "img/logo.svg",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "cdpsSidebar",
+          position: "left",
+          label: "Swift",
+        },
+        {
+          href: "https://github.com/NKUST-iOS-Club/docs",
+          label: "GitHub",
+          position: "right",
+        },
+        {
+          type: "localeDropdown",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "技術文件",
+          items: [
+            {
+              label: "Swift",
+              to: "/docs/swift/start/",
+            },
+          ],
+        },
+        {
+          title: "社群連結",
+          items: [
+            {
+              label: "Discord",
+              href: "https://discord.gg/pdYbhFBAj8",
+            },
+          ],
+        },
+        {
+          title: "其他連結",
+          items: [
+            {
+              label: "部落格",
+              to: "/blog",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/NKUST-iOS-Club",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} ExpTech Studio. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.oneLight,
+      darkTheme: prismThemes.oneDark,
+      additionalLanguages: ["bash"],
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
